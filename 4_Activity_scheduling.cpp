3788af17e8473d70b2e13a72c71d4acb8d5489a5
karim-hossion <karimhossion2012@gmail.com>
2024-12-04 00:01:21 +0600

    karim

--- 4_Activity_scheduling.cpp
+++ 4_Activity_scheduling.cpp
@@ -1,55 +1,75 @@
-// C++ program for activity selection problem.
-
-// The following implementation assumes that the activities
-// are already sorted according to their finish time
 #include <bits/stdc++.h>
 using namespace std;
-
-// Prints a maximum set of activities that can be done by a
-// single person, one at a time.
-void printMaxActivities(int s[], int f[], int n)
+void sorting (int name[], int s[], int f[], int n)
 {
-    int i, j;
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (f[i]>f[j])
+            {
+                temp = name[i];
+                name[i]= name[j];
+                name[j]=temp;
 
-    cout << "Following activities are selected" << endl;
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
 
-    // The first activity always gets selected
-    i = 0;
-    cout << i+1 << " ";
-
-    // Consider rest of the activities
-    for (j = 1; j < n; j++) {
-        // If this activity has start time greater than or
-        // equal to the finish time of previously selected
-        // activity, then select it
-        if (s[j] >= f[i]) {
-            cout << j << " ";
-            i = j;
+                temp = f[i];
+                f[i] = f[j];
+                f[j] = temp;
+            }
         }
     }
 }
 
-// Driver code
-int main()
+void activity (int name[], int s[], int f[], int n)
 {
-    cout << "Enter the number of activities: " << endl;
-    int n;
-    cin>>n;
-    int s[n];
-    int f[n];
-    cout << "Start times: ";
-    for (int i = 0; i < n; i++)
+    int act[n], act_count=0;
+
+    act[act_count]=name[0];
+    int k=0;
+    for (int i = 1; i < n; i++)
     {
-        cin >> s[i];
+        if (s[i]>=f[k])
+        {
+            act[++act_count]=name[i];
+            k=i;
+        }
     }
 
-    cout << "Finish times: ";
+    cout<<"Activity name : ";
+    for (int i = 0; i <= act_count; i++)
+    {
+        cout<<act[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter number of activity : ";
+    cin>>n;
+
+    int name[n], s[n],f[n];
+
+    cout<<"Enter name of activity : ";
     for (int i = 0; i < n; i++)
     {
-        cin >> f[i];
+        cin>>name[i];
     }
-    
-    // Function call
-    printMaxActivities(s, f, n);
-    return 0;
+    cout<<"Enter start of activity : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>s[i];
+    }
+    cout<<"Enter finish of activity : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>f[i];
+    }
+    sorting(name, s,f,n);
+    activity(name, s, f, n);  
 }
--- 8.Longest.cpp
+++ 8.Longest.cpp
@@ -1,76 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-void lcsAlgo(char *s1,char *s2,int m,int n)
+void lcsAlgom(char *s1, char *s2, int m, int n)
 {
-    int lcsTable[m+1][n+1];
-
-    for(int i=0;i<=m;i++)
+    int lcsTable[m + 1][n + 1];
+    for (int i = 0; i <= m; i++)
     {
-        for(int j=0;j<=n;j++)
+        for (int j = 0; j <= n; j++)
         {
-            if(i==0 || j==0)
+            if (i == 0 || j == 0)
             {
-                lcsTable[i][j]=0;
+                lcsTable[i][j] = 0;
             }
-            else if(s1[i-1]==s2[j-1])
+            else if (s1[i - 1] == s2[j - 1])
             {
-                lcsTable[i][j]=lcsTable[i-1][j-1]+1;
+                lcsTable[i][j] = lcsTable[i - 1][j - 1] + 1;
             }
             else
             {
-                lcsTable[i][j]=max(lcsTable[i-1][j],lcsTable[i][j-1]);
+                lcsTable[i][j] = max(lcsTable[i - 1][j], lcsTable[i][j - 1]);
             }
         }
     }
 
-    int lcs_length=lcsTable[m][n];
-    char lcs[lcs_length+1];
-    lcs[lcs_length]='\0';
+    int lcs_length = lcsTable[m][n];
+    char lcs[lcs_length + 1];
+    lcs[lcs_length] = '\0';
 
-    int i=m,j=n;
-
-    while(i>0 && j>0)
+    int i = m, j = n;
+    while (i > 0 && j > 0)
     {
-        if(s1[i-1]==s2[j-1])
+        if (s1[i - 1] == s2[j - 1])
         {
-            lcs[lcs_length-1]=s1[i-1];
+            lcs[lcs_length - 1] = s1[i - 1];
             i--;
             j--;
             lcs_length--;
         }
-        else if(lcsTable[i-1][j]>lcsTable[i][j-1])
+        else if (lcsTable[i - 1][j] > lcsTable[i][j - 1] - 1)
+        {
             i--;
+        }
         else
+        {
             j--;
+        }
     }
-
-    lcs_length=lcsTable[m][n];
-    cout<<"The first sequence:"<<s1<<"\nThe second sequence:";
-    //s2[n]='\0';
-    cout<<s2<<endl;
-    cout<<"The LCS:"<<lcs<<endl;
-    cout<<"LCS length:"<<lcs_length;
+    lcs_length = lcsTable[m][n];
+    cout << "The Lcs : " << lcs << endl;
+    cout << "LCS length : " << lcs_length;
 }
-
 int main()
-{   int m=0;
-    int n=0;
-    cout<<"The length of 1st sequence:";
+{
+    int m=0,n=0;
+    cout<<"Enter length of 1st sequence : ";
     cin>>m;
-    cout<<"The first sequence:";
+    cout<<"Enter first sequence : ";
     char s1[m];
-    for(int i=0;i<m;i++)
+    for (int i = 0; i < m; i++)
     {
         cin>>s1[i];
     }
-    cout<<"The length of 2nd sequence:";
+
+    cout<<"Enter length of 2nd sequence : ";
     cin>>n;
-    cout<<"The second sequence:";
+    cout<<"Enter second sequence : ";
     char s2[n];
-    for(int j=0;j<n;j++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>s2[j];
+        cin>>s2[i];
     }
-    lcsAlgo(s1,s2,m,n);
+
+    lcsAlgom(s1, s2, m, n);
+    
+    
 }
